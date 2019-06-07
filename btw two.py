b1b1b7cafd774ecfe023a3ecf78eb6592bf28d84
subashbabu97 <48273911+subashbabu97@users.noreply.github.com>
2019-06-07 10:03:41 +0530

    Update btw two.py

--- btw two.py
+++ btw two.py
@@ -5,6 +5,5 @@ for i in range(s+1,e):
         for j in range(2,i):
                 if i%j==0:
                         break
-                else:
-                        print(i,end=' ')
+         print(i,end=' ')
                        
